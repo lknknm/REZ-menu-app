0eac5d390b394b5c49a7cb9a8370d96b0d3a5b72
lknknm <108239558+lknknm@users.noreply.github.com>
2023-08-14 23:05:41 -0300

    Add Item Dialog Page

--- REZ/FoodMenu.xaml.cs
+++ REZ/FoodMenu.xaml.cs
@@ -13,6 +13,7 @@ using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Microsoft.UI.Xaml.Media.Animation;
+using System.Threading.Tasks;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -60,10 +61,22 @@ namespace REZ
         {
             ToggleThemeTeachingTip1.IsOpen = true;
         }
-        }
-        private void ToggleListTip(object sender, RoutedEventArgs e)
+        private async void ToggleListTip(object sender, RoutedEventArgs e)
         {
-            ItemTeachingTip.IsOpen = true;
+            ContentDialog dialog = new ContentDialog();
+
+            // XamlRoot must be set in the case of a ContentDialog running in a Desktop app
+            dialog.XamlRoot = this.XamlRoot;
+            dialog.Style = Application.Current.Resources["DefaultContentDialogStyle"] as Style;
+            dialog.Title = "Product 1";
+            dialog.PrimaryButtonText = "Save";
+            dialog.SecondaryButtonText = "Don't Save";
+            dialog.CloseButtonText = "Cancel";
+            dialog.DefaultButton = ContentDialogButton.Primary;
+            dialog.Content = new ItemDialogModal();
+
+            var result = await dialog.ShowAsync();
+
         }
         private void myListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
